9378454f8169f1b9d367d96649fb907a48087bcf
DotShark <arnaud.petit4@gmail.com>
2025-03-21 10:07:26 +0100

    Nuxt setup

---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="w-full h-screen flex flex-col items-center justify-center p-0 sm:px-8 md:px-16 lg:px-32 xl:px-64">
+    <div class="w-full aspect-video bg-gray-800" />
+  </div>
+</template>
